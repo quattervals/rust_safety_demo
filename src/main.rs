373819ede7f5f7ed3fe1d4fc081314aa5b9d60ec
Oliver With <oliverwith@bbv.ch>
2024-12-10 11:37:42 +0100

    RAII enforcement

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,68 @@
 //! This file intended to show rust memory safety mechanisms.
 //!
-//! For more information, please refer to the following resources:
-//!
-//! - Examples are derived from [rust by example](https://doc.rust-lang.org/rust-by-example/scope.html)
+//! Examples are derived from [rust by example](https://doc.rust-lang.org/rust-by-example/scope.html)
+
+/// Demo the borrow checker with refernces
+/// Demo derived from [rust by example (borrowing section)](https://doc.rust-lang.org/rust-by-example/scope/borrow/alias.html)
+///
+/// Borrowing Rules
+/// - Data can be immutably borrowed any number of times.
+/// - Data can be mutably borrowed only once at a time i.e. there is exactly this one mutable reference.
+/// - After the mutable borrow goes out of scope, the immutable borrows can be used again.
+///     - or another mutable borrow can be created.
+///
+/// It may take some getting used to that the "scope" of a reference ends with its last usage and not with actual scopes.
+fn demo_references() {
+    let mut point = Point { x: 12, y: -4, z: 8 };
+
+    let borrowed_point = &point;
+    let other_borrowed_point = &point;
+
+    // Can't mutably borrow at this point because the references are still in use in the println!() statement.
+    //let mut mutable_borrowed_point = &mut point;
+
+    println!(
+        "Point has coordinates: ({}, {}, {})",
+        borrowed_point.x, other_borrowed_point.y, point.z
+    );
+
+    // Now we can mutably borrow
+
+    // ... in a function
+    mutably_borrow_point(&mut point);
+
+    // ... and in the same scope
+    let mutable_borrowed_point = &mut point;
+    mutable_borrowed_point.x += 2;
+
+    // Can't have any borrow at this point because the references are still in use in the println!() statement.
+    //let borrowed_point = &point;
+
+    println!(
+        "Altered Point has coordinates: ({}, {}, {})",
+        mutable_borrowed_point.x, mutable_borrowed_point.y, mutable_borrowed_point.z
+    );
+}
+
+fn mutably_borrow_point(point: &mut Point) {
+    point.z += 1;
+}
+
+/// show how RAII is enforced
+/// (RAII is still a horrible name. What about "Resource Lifecycle Management"?)
+/// Demo derived from [rust by example (raii section)](https://doc.rust-lang.org/rust-by-example/scope/raii.html)
+///
+/// There is no way you can put something on the heap without managing it.
+fn demo_raii_is_enforced() {
+    let _box_1 = Box::new(52_i32);
+
+    {
+        let _box_2 = Box::new(53_i32);
+
+        // `_box_2` is popped off the stack here. The "destructor" cleans up the heap memory.
+        // You'll notice: the behaviour is very similar to `unique_ptr` in C++
+    }
+}
 
 fn demo_by_value_for_primitives() {
     let x = 5_u32;
@@ -28,18 +88,17 @@ fn consume_box(c: Box<i32>) {
 }
 
 /// Rust moves ownership (even though this looks like 'by value', which would be a copy)
-fn consume_value(s: SimpleStruct) {
-    println!("Consumed complex value: a {} b {}", s.a, s.b);
+fn consume_value(s: Point) {
+    println!("Consumed complex value: x {} y {} z {}", s.x, s.y, s.z);
 }
-
-#[derive(Debug)]
-struct SimpleStruct {
-    a: i32,
-    b: i32,
+struct Point {
+    x: i32,
+    y: i32,
+    z: i32,
 }
 
 fn demo_moved_ownership() {
-    let simple_struct = SimpleStruct { a: 1, b: 2 };
+    let simple_struct = Point { x: 1, y: 2, z: -4 };
 
     consume_value(simple_struct);
 
@@ -64,8 +123,8 @@ fn demo_moved_ownership() {
 }
 
 fn main() {
+    demo_raii_is_enforced();
     demo_by_value_for_primitives();
-
     demo_moved_ownership();
-
+    demo_references();
 }
